66aa5461484c044a1b6a665f494e6dccf3fbd7cc
Kennyhunter254 <kipronokevin566@gmail.com>
2024-06-29 01:42:06 +0300

    arithmetic lab code

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+// Define num1 and num2 for multiplication
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// Generate a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+ // Adjust range as needed
+
+// Define num3 and num4 for modulo operation
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+// Find the maximum number in a set
+const max = Math.max(1, 5, 10, 15, 20);
